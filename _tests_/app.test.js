ee0ce40c3b50c591ddfd32499d07c4cab8757d90
Daniel Bennington <dbennington125@gmail.com>
2020-06-04 16:29:28 -0700

    list done

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Dog = require('../lib/models/Dog');
 
 describe('app routes', () => {
   const mongo = new MongoMemoryServer();
@@ -20,6 +21,62 @@ describe('app routes', () => {
   afterAll(()=> {
     return mongoose.connection.close();
   });
+  
+  it('creates a new dog', () => {
+    return request(app)
+      .post('/dogs')
+      .send({
+        name: 'Spot',
+        age: 5,
+        color: 'red'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Spot',
+          age: 5,
+          color: 'red',
+          __v: 0
+        });
+      });
+  });
+
+  it('creates a list of dogs', async() => {
+    await Dog.create([
+      {
+        name: 'Spot',
+        age: 5,
+        color: 'red'
+      },
+      {
+        name: 'Rover',
+        age: 6,
+        color: 'blue'
+      }
+    ]);
+    return request(app)
+      .get('/dogs')
+      .then(res => {
+        expect(res.body).toEqual(
+          [
+            {
+              _id: expect.anything(),
+              name: 'Spot',
+              age: 5,
+              color: 'red',
+              __v: 0
+            },
+            {
+              _id: expect.anything(),
+              name: 'Rover',
+              age: 6,
+              color: 'blue',
+              __v: 0
+            },
+          ]);
+      });
+  });
+
   it('creates a new dog', () => {
     return request(app)
       .post('/dogs')
