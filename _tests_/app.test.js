4469c4b671ab867bf0f70f0144c83fd0458cdca1
Daniel Bennington <dbennington125@gmail.com>
2020-06-04 15:15:21 -0700

    schema added

---
+++ _tests_/app.test.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+describe('app routes', () => {
+  const mongo = new MongoMemoryServer();
+  beforeAll(() => {
+    return mongo.getUri()
+      .then(uri => mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }));
+  });
+
+  beforeEach(() =>{
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(()=> {
+    return mongoose.connection.close();
+  });
+  it('creates a new dog', () => {
+    
+  });
+});
